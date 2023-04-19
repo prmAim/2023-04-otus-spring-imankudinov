32821b153d9f2e20bccdeddfa18f043a64e89b97
iankudinov <iankudinov@diasoft.ru>
2023-04-19 12:32:26 +0500

    Spring 01 Домашнее задание у уроку 1 Введение_в_Spring_Framework Работа над ошибками

--- spring-01-ioc/spring-01-exercise/src/main/java/ru/otus/spring/service/LoadResource.java
+++ spring-01-ioc/spring-01-exercise/src/main/java/ru/otus/spring/service/LoadResource.java
@@ -1,12 +1,8 @@
 package ru.otus.spring.service;
 
-import org.springframework.core.io.Resource;
-
-import java.util.List;
-
 public interface LoadResource {
   /**
    *  Загрузка ресурсов
    */
-  public String loadFile();
+  String loadFile();
 }
